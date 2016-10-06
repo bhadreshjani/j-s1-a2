4f0be3cd6942ae1036b44ca5c9e06cce72219107
bhadreshjani <nilkanthim@gmail.com>
2016-10-06 02:34:24 -0700

    Add files via upload

---
+++ operatortry.java
@@ -0,0 +1,15 @@
+
+public class operatortry {
+	public static void main(String arg[])
+	{
+	int a=20,b=10,c,d,e;    //all variable declaration
+	b=a-- - --a;
+	System.out.println(b);
+	c=a--;
+	System.out.println(c);
+	d=a>>2;
+	System.out.println(d);
+	e=a&b;
+	System.out.println(e);
+	}
+}
